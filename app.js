dd43175690e1e0f5fcf1627eeef4a8a2991e4b22
Thrivikram teja <thrivikramteja.2006@gmail.com>
2025-03-11 22:10:44 +0530

    made some changes

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 const path = require("path");
-const http = require('http')
 
 const express = require("express");
-const router = express.Router();
+
+const baseRoutes = require("./routes/base.routes");
+const authRoutes = require("./routes/auth.routes.js");
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-
-app.get('/', (req, res) => {
-    res.render('landing_page');
-});
+app.use(baseRoutes);
+app.use(authRoutes);
 
 app.listen(3000, () => console.log("Server running on port 3000"));
-
--- routes/base.routes.js
+++ routes/base.routes.js
@@ -1,4 +1,9 @@
 const express = require("express");
+
 const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('users/landing-page');
+});
 
 module.exports = router;
