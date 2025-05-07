531be8abcc8cf22f026faf2e811bd101e9a844f3
Thrivikram teja <thrivikramteja.2006@gmail.com>
2025-05-07 09:36:46 +0530

    changed NGO routes

--- routes/NGO.routes.js
+++ routes/NGO.routes.js
@@ -12,15 +12,12 @@ router.post('/registerNgo', NGOController.register);
 
 router.get('/events', NGOController.getEvents);
 
-<<<<<<< HEAD
 router.get('/registerUser/:ngoID/:eventName', isAuth, NGOController.getRegisterUser);
 
 router.post('/registerUser/:ngoID', isAuth, NGOController.registerUser);
 
-router.get('/fundraisers', NGOController.getFundraisers);
-=======
+// router.get('/fundraisers', NGOController.getFundraisers);
 router.get('/fundraisers', NGOController.getallFundraisers);
->>>>>>> 1fb3730c8d47466a6b0a787c951e7b95afa94caf
 
 router.get('/NGO-dashboard/:ngoID', isAuth, NGOController.getNGO);
 
