a851bd0ce4fea76694eb652839070ca3f5b5c3a6
karthik2207babu <chinnikarthik22@gmail.com>
2026-04-20 10:45:50 +0530

    All case sensitivities are checked

--- Frontend/src/components/carousel.jsx
+++ Frontend/src/components/carousel.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import styles from "./Carousel.module.css"; 
+import styles from "./carousel.module.css"; 
 
 const Carousel = () => {
     const slides = [
