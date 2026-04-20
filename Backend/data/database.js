7e4ecfc57da1ec74bd6ef24d6abdf6180508c75c
karthik2207babu <chinnikarthik22@gmail.com>
2026-04-20 09:09:15 +0530

    Database String Changed

--- Backend/data/database.js
+++ Backend/data/database.js
@@ -1,5 +1,10 @@
 const mongoose = require("mongoose");
 
-const db = mongoose.connect("mongodb://127.0.0.1:27017/FSDProject");
+
+const dbPath = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/FSDProject";
+
+const db = mongoose.connect(dbPath)
+  .then(() => console.log("Successfully connected to Database"))
+  .catch((err) => console.error("Database connection error:", err));
 
 module.exports = { db };
